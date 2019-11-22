244ea7b785b5b613428c61a3afb66dd22f6bcab7
Kent Hervey <kent.hervey@gmail.com>
2019-11-22 15:46:19 -0600

    clarify instanceof, removed  getContainingType

--- src/com/hervey/app/Driver.java
+++ src/com/hervey/app/Driver.java
@@ -9,10 +9,15 @@ public class Driver {
 		Animal dogAnimal = new Dog();
 		
 		System.out.println("Code line:  Animal dogAnimal = new Dog();");
-		System.out.println("Is dogAnimal an instance of the Reference Type Animal?  " + (dogAnimal instanceof Animal));
-		System.out.println("Is dogAnimal an instance of the Object Type Dog?  " + (dogAnimal instanceof Dog) + "\n");
-		System.out.println("the getClass of dogAnimal is:  " + dogAnimal.getClass());
-		//System.out.println("Declared type of dogAnimal is  " + getContainingType(dogAnimal));
+		System.out.println("Thus, Animal is the Declared Type; Dog is the object type--aka run-type or actual type\n");
+		
+		System.out.println("Using \"instanceof\"");
+		System.out.println("Is dogAnimal an instance of type Object?  " + (dogAnimal instanceof Object));
+		System.out.println("Is dogAnimal an instance of type Animal?  " + (dogAnimal instanceof Animal));
+		System.out.println("Is dogAnimal an instance of type Dog?  " + (dogAnimal instanceof Dog) );
+		System.out.println("Thus, instanceof does not tell us the declared type, or object type, but if we test for either, it will yeild true.\n");
+		System.out.println("The getClass name of dogAnimal is:  " + dogAnimal.getClass().getName());
+		System.out.println("This is the object type or run-type");
 		
 
 	}
